56fe65027240df285302a772d9d4b44c2c19519a
Senorsen <sen@senorsen.com>
2014-09-19 09:21:07 +0800

    Change DNS, fixed & close #4

--- dns.sh
+++ dns.sh
@@ -16,10 +16,9 @@ zju_test_and_set_up_dns () {
 }
 
 # 浙大的DNS不是很稳定，这里列了备用方案
-# 官方 > 10.12.10.12 > Senorsen DNS > RQ Lab > ALI YUN
+# 官方 > 10.12.10.12 > Senorsen (QSC Server) DNS > ALI YUN
 zju_test_and_set_up_dns \
     10.10.0.21 10.10.0.22 10.10.0.23 \
     10.12.10.12 \
-    10.76.8.111 \
-    10.202.68.222 \
+    10.202.68.43 \
     223.5.5.5 223.6.6.6 
