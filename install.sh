7a103f4b06a9f90ed3ed83811b09bfb3fafa36e2
Zeno Zeng <zenoofzeng@gmail.com>
2014-09-26 14:05:58 +0800

    add install.sh, fixes #21

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+mkdir -p /usr/local/lib/zjunet
+cp -rf $BASEDIR /usr/local/lib/zjunet
+echo "#!/bin/sh" > /usr/local/bin/zjunet
+echo "/usr/local/lib/zjunet/zjunet.sh $@" >> /usr/local/bin/zjunet
+chmod +x /usr/local/bin/zjunet
+echo "[INFO] Done."
+echo
+zjunet
