b81ecf68e960807011223fa7edbf1925199b0248
Zeno Zeng <zenoofzeng@gmail.com>
2014-09-29 19:52:09 +0800

    fix typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname $0)/lib
 mkdir -p /usr/local/lib/zjunet
-cp -rf $BASEDIR /usr/local/lib/zjunet
+cp -rf $BASEDIR/* /usr/local/lib/zjunet
 mkdir -p /usr/local/bin
 echo "#!/bin/sh" > /usr/local/bin/zjunet
 echo '/usr/local/lib/zjunet/zjunet.sh "$@"' >> /usr/local/bin/zjunet
