08cfad66d1d8b4ba1eca579b13b9b4eb81963925
Zeno Zeng <zenoofzeng@gmail.com>
2014-09-29 18:34:45 +0800

    mkdir -p /usr/local/bin when install

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 BASEDIR=$(dirname $0)
 mkdir -p /usr/local/lib/zjunet
 cp -rf $BASEDIR /usr/local/lib/zjunet
+mkdir -p /usr/local/bin
 echo "#!/bin/sh" > /usr/local/bin/zjunet
 echo '/usr/local/lib/zjunet/zjunet.sh "$@"' >> /usr/local/bin/zjunet
 chmod +x /usr/local/bin/zjunet
 echo "[INFO] Done."
 echo
-zjunet
+zjunet usage
