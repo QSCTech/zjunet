f83f10238466d2e1735c4b3009d9dcf88fcc98fc
Zeno Zeng <zenoofzeng@gmail.com>
2014-09-30 16:08:55 +0800

    build/debian/control

--- build/debian/build.sh
+++ build/debian/build.sh
@@ -1,6 +1,19 @@
-VERSION=0.0.57
-PKG=zjunet-${VERSION}
-mkdir -p ${PKG}
-tar cvzf ${PKG}.tar.gz ../../lib/
-cd ${PKG}
-dh_make -e zenoofzeng@gmail.com -f ../${PKG}.tar.gz
+#!/bin/sh
+
+# bin
+mkdir -p ./debian/usr/bin
+cp -r ../../lib ./debian/usr
+echo '/usr/lib/zjunet/zjunet.sh "$@"' >> ./debian/usr/bin/zjunet
+chmod +x ./debian/usr/bin/zjunet
+
+# contorl file
+mkdir -p debian/DEBIAN
+find ./debian -type d | xargs chmod 755
+cp control debian/DEBIAN
+
+# dpkg-deb
+dpkg-deb --build debian
+mv debian.deb zjunet_0.1-1_all.deb
+
+# remove debian/
+rm -rf ./debian
